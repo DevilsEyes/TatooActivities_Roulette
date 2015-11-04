c55fd8e285ecb243ccec591665d8c089a4ca139b
DevilsEyes <DevilsEyes@git.com>
2015-11-04 15:31:23 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -32,6 +32,10 @@ var ex = {
             },
             error: obj.error ? obj.error : function () {
                 layer.msg('您的网络连接不太顺畅哦!');
+                template.render('PTest','page',{
+                    url:obj.url,
+                    data:ex.params(obj.data,'html')
+                })
             },
             beforeSend: obj.beforeSend ? obj.beforeSend : function () {
                 layer.load(2);
@@ -42,6 +46,51 @@ var ex = {
                 pageObj ? pageObj.Locked = false : null;
             }
         })
+    },
+    params: function (obj, type) {
+        var ws, br;
+        switch (type) {
+            case 'html':
+                ws = '&nbsp;';
+                br = '<br>';
+                break;
+            case 'log':
+                ws = ' ';
+                br = '\n';
+                break;
+        }
+
+        function space(i) {
+            if (i == 1)return ws;
+            else {
+                return ws + space(i - 1);
+            }
+        }
+
+        var str = '';
+        if (typeof obj != 'object') {
+            str += obj + space(3) + '[TYPE:' + typeof obj + ']';
+        } else {
+            for (var i in obj) {
+                str += space(1) + i + ': ' + obj[i] + br;
+
+                if (typeof obj[i] == 'object') {
+                    for (var j in obj[i]) {
+                        str += space(5) + '└─' + space(1) + j + ': ' + obj[i][j] + br;
+
+                        if (typeof obj[i][j] == 'object') {
+                            for (var k in obj[i][j]) {
+                                str += space(10) + '└─' + space(1) + k + ': ' + obj[i][j][k] + br;
+                            }
+                            str += br;
+                        }
+                    }
+                    str += br;
+                }
+            }
+            str += br;
+        }
+        return str;
     }
 };
 
